b29ff68b13030569c6d6527bca0faa5e1da25ca4
jrantwijk <jrantwijk@swis.nl>
2015-02-23 13:37:39 +0100

    initial commit

---
+++ src/VictoryCms/TestPackage/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jrantwijk
+ * Date: 23-2-2015
+ * Time: 13:35
+ */
+
+namespace VictoryCms\TestPackage;
+
+
+class Test
+{
+    function __construct()
+    {
+    }
+}
