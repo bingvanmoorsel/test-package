6d26f6ddded4f130d39d92b2da69ed65029f739f
Bing van Moorsel <bvanmoorsel@swis.nl>
2015-03-31 11:40:15 +0200

    demo fix

--- src/VictoryCms/Testpackage/Http/Controllers/HomeController.php
+++ src/VictoryCms/Testpackage/Http/Controllers/HomeController.php
@@ -1,23 +1,12 @@
 <?php namespace VictoryCms\Testpackage\Http\Controllers;
 
-
 use Illuminate\Routing\Controller;
-use VictoryCms\Core\Form\Elements\Label;
-use VictoryCms\Core\Form\Elements\Password;
-use VictoryCms\Core\Form\Elements\Text;
-use VictoryCms\Core\Form\Form;
 
 class HomeController extends Controller {
 
     public function index()
     {
-        $form = new Form();
-        $form->add(new Label('name', 'Naam'));
-        $form->add(new Text('name'));
-        $form->add(new Label('password', 'Wachtwoord'));
-        $form->add(new Password('password'));
-
-        return view('victory.testpackage::home', compact('form'));
+        return view('victory.testpackage::home');
     }
 
 }
--- src/VictoryCms/Testpackage/Http/routes.php
+++ src/VictoryCms/Testpackage/Http/routes.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * When creating a route its relative to the root of this package:
+ * http://<YourBaseUrl>/victory/victory-cms/test-package
+ */
 $this->routes(function($router){
 
     $router->get('/', 'VictoryCms\TestPackage\Http\Controllers\HomeController@index');
 
+});
+
+
+/*
+ * When creating a route outside of your own package you register the routes to the victory routes:
+ * http://<YourBaseUrl>/victory/test
+ */
+$this->app->victory->routes('test', function($router){
+    $router->get('/', 'VictoryCms\TestPackage\Http\Controllers\HomeController@index');
 });
